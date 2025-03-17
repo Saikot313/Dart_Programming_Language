58fde716b0aa2c6ec641070d70be697d5d0ae523
Sakender Saikot <sakendersaikot88@gmail.com>
2025-03-18 00:14:48 +0600

    Dart List

---
+++ List.dart
@@ -0,0 +1,20 @@
+void main(){
+  List<String> names = ["Saikot","Pranto","Abir","Rahik"];
+  List<String> universities = ["VU","BAUET","BAUET","RUET"];
+  List<int> ages = [25, 26, 31, 22];
+  List<double> heights = [5.8, 5.7, 5.6, 5.6];
+  List<bool> isStudents = [false, true, false,false];
+  print("Names: $names");
+  print("University: $universities");
+  print("Ages: $ages");
+  print("Heights: $heights");
+  print("Is Students: $isStudents");
+
+  // Accessing List Elements//
+
+  print("First Name: ${names[0]}");
+  print("Second Name: ${names[1]}");
+  print("Third Name: ${names[2]}");
+  print("Fourth Name: ${names[3]}");
+
+}
