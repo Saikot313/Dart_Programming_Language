0b3cf38fc64a02d6e2c60f81a34bf41229adcfc4
Sakender Saikot <sakendersaikot88@gmail.com>
2025-03-18 00:08:30 +0600

    Dart Varibale

---
+++ dart_variables.dart
@@ -0,0 +1,11 @@
+ void main(){
+  String name = "Md.Sakender Saikot";
+  int age = 25;
+  double height = 5.8;
+  bool isStudent = false;
+  print("Name: $name");
+  print("Age: $age");
+  print("Height: $height");
+  print("Is Student: $isStudent");
+
+ }
